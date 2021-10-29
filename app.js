bd84734c286694a2d9d250e2f2e192538b5efef4
tanukin <tantantateipi@gmail.com>
2021-10-29 20:49:01 +0900

    連想配列による高速化

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 'use strict';
+const fibMap = new Map();
+fibMap.set = (0,0);
+fibMap.set = (1,1);
+
+function fib(n){
+    if(fibMap(n).has){
+        return fibMap.get(n);
+    }
+    const value = fib(n-1) + fib(n-2);
+    fibMap.set = (n,value);
+    return value;
+}
+
+const length = 40;
+for(let i =0; i <= length; i++){
+    console.log(fib(i));
+}
