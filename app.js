cd6c715f8a480b935c728870616a63fc28cdb095
YURI <taro0koma@gmail.com>
2022-04-11 17:12:47 +0900

    メモ化してトリボナッチ数列を出力するコード

--- app.js
+++ app.js
@@ -11,6 +11,7 @@
 const memo = new Map();
 memo.set(0, 0);
 memo.set(1, 1);
+memo.set(2, 1);
 
 function fib(n) {
     if (memo.has(n)) {
@@ -22,7 +23,17 @@ function fib(n) {
     return value;
 }
 
+function trib(n) {
+    if (memo.has(n)) {
+        return memo.get(n);
+    }
+    const value = trib(n - 1) + trib(n - 2) + trib(n - 3);
+    memo.set(n, value);
+    return value;
+}
+
 const length = 40;
 for (let i = 0; i <= length; i++) {
-    console.log(fib(i));
+    // console.log(fib(i));
+    console.log(trib(i));
 }
