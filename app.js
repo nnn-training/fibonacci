9891b27d430534d7e93511d77a88e8ab856e4e6c
清水杏奈 <a.shimizu@ultinet.co.jp>
2022-04-21 16:59:10 +0900

    トリボナッチ

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
 'use strict';
+const memo = new Map();
+//memo.setで条件を先に設定。
+memo.set(0, 0);
+memo.set(1, 1);
+//memo.hasで持っているか確認。持っていたらreturnへ
 function fib(n) {
-  if (n === 0) {
-    return 0;
+  if (memo.has(n) === true) {
+    return memo.get(n);
   }
-  if (n === 1) { //else if 使わなくてもいい。(returnしてるからっぽい？)
-    return 1;
-  }
-  return fib(n - 1) + fib(n - 2);
+//値がなかったので計算、memo.setで計算した値をMapに登録後出た答えを返す。
+  const value = fib(n - 1) + fib(n - 2);
+  memo.set(n, value);
+  return value;
 }
 const length = 40;
 for (let i = 0; i <= length; i++) {
   console.log(fib(i));
 }
+
+/*
+/ const memo = new Map();
+これでMapのオブジェクトができる。
+ new Map();をここではmemoという変数に代入して、
+ データの追加：
+ 変数名.set(第一引数にキー(数でも文字でも),第二引数には値);で,
+定義できる。（記憶させる）
+データの参照：
+変数名.get();でできる。
+
+/　if (memo.has(n) === true) {}
+hasで、すでにmemoに値が登録されているか確認できる。
+trueかfalseを返してくれる。
+ここではもしmemoが持っていたらtrueなので、
+データの参照＝return memo.get(n)への処理へはいる。
+
+
+*/
